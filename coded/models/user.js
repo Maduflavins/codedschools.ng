cdd65b5ac183583d1035ce0d07f5673cdfe03ed5
Flavins <maduabuchiokonkwo@gmail.com>
2017-12-01 02:15:42 +0100

    final touch on profiles

--- coded/models/user.js
+++ coded/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -38,6 +39,13 @@ UserSchema.pre('save', function(next){
 //Compare password
 UserSchema.methods.comparePassword = function(password){
   return bcrypt.compareSync(password, this.password)
+}
+
+UserSchema.methods.gravatar = function(size){
+  if(!this.size) size = 200;
+  if(!this.email) return 'https://gravatar.com/avartar/?s' + size + '&d=retro';
+  var md5 =crypto.createHash('md5').update(this.email).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
 }
 
 
--- coded/routes/user.js
+++ coded/routes/user.js
@@ -38,6 +38,7 @@ router.post('/signup', (req, res, next) =>{
   user.profile.firstname = req.body.firstname;
   user.profile.lastname = req.body.lastname;
   user.profile.coded = req.body.coded;
+  user.profile.picture = user.gravatar();
 
   User.findOne({email: req.body.email}, function(err, existingUser){
     if(exstingUser){
@@ -46,7 +47,10 @@ router.post('/signup', (req, res, next) =>{
     }else{
       user.save().then((newuser) =>{
         req.flass('success', 'successfully created an account');
-        return res.redirect('/');
+        req.logIn(user, (err)=>{
+          if(err) return next(err)
+          res.redirect("/profile");
+        })
       }, (e) =>{
         req.flash('errors', 'unable to create account');
         return res.rediredt('/signup');
@@ -56,6 +60,27 @@ router.post('/signup', (req, res, next) =>{
   })
 
 });
+router.get("/logout", (req, res, next)=>{
+  req.logout();
+  res.redirect("/");
+})
 
+router.get("/edit-profile", (req, res, next)=>{
+  res.render("accounts/edit-profile", {message: req.flass('success')});
+})
+
+router.post("/edit-profile", (req, res, next)=>{
+  User.findOne({_id: req.user._id},(err, user)=>{
+    if(req.body.firstname)user.profile.firstname = req.body.firstname;
+    if(req.body.lastname) user.profile.lastname = req.body.lastname;
+    if(req.body.address) user.address = req.body.address;
+
+    user.save((err)=>{
+      if(err) return next(err);
+      req.flash('success', 'successfully edited your profile');
+      return res.redirect('/profile');
+    })
+  })
+})
 
 module.exports = router;
